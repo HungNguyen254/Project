0024945e8660d02e6b1742bb826f3c411117dc2e
HungNguyen254 <nguyenhung2540206@gmail.com>
2026-04-01 22:03:38 +0700

    Add files via upload

---
+++ Project-Thương Mại Điện Tử/JS/category.js
@@ -0,0 +1,13 @@
+function UpformAddcategory(){
+    document.querySelector(".FormAddCategory").style.display = "block"
+}
+function CloseformAddcategory(){
+    document.querySelector(".FormAddCategory").style.display = "none"
+
+}
+function UpFormUpdateCategory(){
+    document.querySelector(".FormUpdateCategory").style.display = "block"
+}
+function CloseFormUpdateCategory(){
+    document.querySelector(".FormUpdateCategory").style.display = "none"
+}
